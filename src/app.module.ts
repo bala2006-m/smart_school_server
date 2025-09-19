f283eadea07b5fc0bbb7d923e9a8c49e4d0db738
Balasubramanian <123bala2006@gmail.com>
2025-09-19 10:26:30 +0530

    bala 19-09-25 10:26

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HolidaysModule } from './holidays/holidays.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { StaffModule } from './staff/staff.module';
+import { StudentsModule } from './students/students.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ClassTimetableModule } from './timetable/class-timetable.module';
+import {FeedbackModule} from './feedback/feedback.module';
+import { SchoolsModule } from './school/schools.module';
+import { ClassesModule } from './class/classes.module';
+import { AdminModule } from './admin/admin.module';
+import {AuthModule } from './auth/auth.module';
+import { AttendanceUserModule } from './attendance-user/attendance-user.module'
+import { MessagesModule } from './messages/messages.module';
+import { LeaveRequestModule } from './leave_request/leave-request.module';
+import { NewsModule } from './news/news.module';
+import { BlockedSchoolModule} from'./blocked-school/blocked-school.module';
+import { UserModule } from './edit-password/user.module';
+import { HomeworkModule } from'./homework/homework.module';
 
 @Module({
-  imports: [],
+  imports: [
+    StudentsModule,
+        HolidaysModule,
+        AttendanceModule,
+         StaffModule,
+         DashboardModule,
+         ClassTimetableModule,
+         FeedbackModule,
+         SchoolsModule,
+         ClassesModule,
+         AdminModule,
+         AuthModule,
+         AttendanceUserModule,
+         MessagesModule,
+         LeaveRequestModule,
+         NewsModule,
+         BlockedSchoolModule,
+         UserModule,
+         HomeworkModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
