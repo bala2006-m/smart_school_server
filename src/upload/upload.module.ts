f14df2c4708b358c452b089bf7252c092d8d5cac
Balasubramanian <123bala2006@gmail.com>
2025-10-29 17:17:58 +0530

    bala 29-10-25 5:17

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
+import { PrismaService } from '../common/prisma.service'; // ✅ Add this line
 
 @Module({
+
   controllers: [UploadController],
+  providers: [PrismaService],
 })
 export class UploadModule {}
