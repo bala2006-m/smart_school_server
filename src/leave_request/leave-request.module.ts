8a03dc596d2eecd230d654619b5300504a6635fa
Balasubramanian <123bala2006@gmail.com>
2025-12-19 18:21:28 +0530

    bala 19-12-25

--- src/leave_request/leave-request.module.ts
+++ src/leave_request/leave-request.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { LeaveRequestController } from './leave-request.controller';
 import { LeaveRequestService } from './leave-request.service';
-import { PrismaService } from 'src/common/prisma.service';
+import { PrismaService } from '../common/prisma.service';
 
 @Module({
   controllers: [LeaveRequestController],
