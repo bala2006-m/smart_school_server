a0b154f8dd8c19cc2df76e46b3344afaca60569a
Balasubramanian <123bala2006@gmail.com>
2025-12-19 18:20:45 +0530

    bala 19-12-25

--- src/leave_request/leave-request.service.ts
+++ src/leave_request/leave-request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import { PrismaService } from 'src/common/prisma.service';
+import { PrismaService } from '../common/prisma.service';
 import * as nodemailer from 'nodemailer';
 
 export type RoleType = 'admin' | 'staff' | 'student';
